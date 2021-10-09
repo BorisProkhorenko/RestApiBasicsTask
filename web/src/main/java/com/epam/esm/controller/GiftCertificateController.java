d46356afc5a882ec8590af5cf93455768d9bcb51
BorisProkhorenko <proxal1304@gmail.com>
2021-10-09 11:59:22 +0300

    documented public API

--- web/src/main/java/com/epam/esm/controller/GiftCertificateController.java
+++ web/src/main/java/com/epam/esm/controller/GiftCertificateController.java
@@ -14,6 +14,17 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * This Controller provides public API for operations with {@link GiftCertificate} entity.
+ * Uses {@link GiftCertificateService} to access Data Base through business-logic layer.
+ * Uses {@link ObjectMapper} to map objects from {@link JsonPatch}
+ *
+ * @author Boris Prokhorenko
+ * @see GiftCertificateService
+ * @see GiftCertificate
+ * @see ObjectMapper
+ * @see JsonPatch
+ */
 @RestController
 @RequestMapping(value = "/certificates")
 public class GiftCertificateController {
@@ -26,16 +37,36 @@ public class GiftCertificateController {
         this.objectMapper = objectMapper;
     }
 
+    /**
+     *Method allows getting {@link GiftCertificate} from DB by its id
+     *
+     * @param id - primary key to search {@link GiftCertificate} entity object in DB
+     * @return {@link GiftCertificate} entity object from DB
+     */
     @GetMapping(value = "/{id}")
     public GiftCertificate getCertificateById(@PathVariable Long id) {
         return service.getCertificateById(id);
     }
 
+    /**
+     *Method allows getting all {@link GiftCertificate} entity objects from DB
+     *
+     * @return {@link List} of {@link GiftCertificate} entity objects from DB
+     */
     @GetMapping(value = "/")
     public List<GiftCertificate> getAllCertificates() {
         return service.getAllCertificates();
     }
 
+    /**
+     * Method allows creating a new {@link GiftCertificate} entity object in DB
+     *
+     * @param name - name field of {@link GiftCertificate}
+     * @param description - description field of {@link GiftCertificate}
+     * @param price - price field of {@link GiftCertificate}
+     * @param duration - duration field of {@link GiftCertificate}
+     * @return {@link GiftCertificate} object, which you created
+     */
     @PostMapping(value = "/{name}/{description}/{price}/{duration}")
     public GiftCertificate createCertificate(@PathVariable String name, @PathVariable String description,
                                       @PathVariable double price, @PathVariable int duration) {
@@ -43,22 +74,46 @@ public class GiftCertificateController {
         return service.createCertificate(certificate);
     }
 
+    /**
+     * Method allows deleting {@link GiftCertificate} from DB by its id
+     *
+     * @param id - primary key to search {@link GiftCertificate} entity object in DB
+     */
     @DeleteMapping(value = "/{id}")
     public void deleteCertificate(@PathVariable Long id) {
         service.deleteCertificate(id);
     }
 
+    /**
+     * Method allows removing {@link com.epam.esm.model.Tag} from {@link GiftCertificate} from DB by theirs id
+     *
+     * @param id - primary key to search {@link GiftCertificate} entity object in DB
+     * @param tagId - primary key to search {@link com.epam.esm.model.Tag} entity object in DB
+     */
     @DeleteMapping(value = "/{id}/{tagId}")
     public void removeTag(@PathVariable Long id, @PathVariable Long tagId) {
         service.removeTag(id, tagId);
     }
 
+    /**
+     * Method allows adding {@link com.epam.esm.model.Tag} to {@link GiftCertificate} in DB by theirs id
+     *
+     * @param id - primary key to search {@link GiftCertificate} entity object in DB
+     * @param tagId - primary key to search {@link com.epam.esm.model.Tag} entity object in DB
+     */
     @PostMapping(value = "/{id}/{tagId}")
     public GiftCertificate addTag(@PathVariable Long id, @PathVariable Long tagId) {
       return service.addTag(id, tagId);
     }
 
-
+    /**
+     * Method allows updating {@link GiftCertificate} info in DB by its id
+     *
+     * @param id - primary key to search {@link GiftCertificate} entity object in DB
+     * @param patch - {@link JsonPatch} from PATCH Http method body which represents
+     *              which fields will be updated
+     * @return {@link ResponseEntity} ok if update succeeded and INTERNAL_SERVER_ERROR if not
+     */
     @PatchMapping(path = "/{id}", consumes = "application/json-patch+json")
     public ResponseEntity<GiftCertificate> updateCustomer(@PathVariable long id,
                                                           @RequestBody JsonPatch patch) {
@@ -80,7 +135,17 @@ public class GiftCertificateController {
         return objectMapper.treeToValue(patched, GiftCertificate.class);
     }
 
-
+    /**
+     * Method allows getting {@link GiftCertificate} objects from db filtered and/or sorted
+     *
+     * @param tagId - if presents filtering by tag id will be applied
+     * @param part - if presents filtering by part of name and description will be applied
+     * @param nameSort - if presents sorting(ASC/DESC) by name will be applied. If nameSort param
+     *                 is different from "asc" or "desc" in ignored case sort will not be applied.
+     * @param descriptionSort- if presents sorting(ASC/DESC) by name will be applied. If nameSort param
+     *      *                 is different from "asc" or "desc" in ignored case sort will not be applied.
+     * @return @return {@link List} of {@link GiftCertificate} entity objects from DB
+     */
     @GetMapping(value = {"/params"})
     public List<GiftCertificate> getCertificatesWithParams(@RequestParam("id") Optional<String> tagId,
                                                     @RequestParam("part") Optional<String> part,
--- web/src/main/java/com/epam/esm/controller/TagController.java
+++ web/src/main/java/com/epam/esm/controller/TagController.java
@@ -1,5 +1,6 @@
 package com.epam.esm.controller;
 
+
 import com.epam.esm.model.Tag;
 import com.epam.esm.service.TagService;
 import org.springframework.http.HttpStatus;
@@ -8,6 +9,14 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 
+/**
+ * This Controller provides public API for operations with {@link Tag} entity.
+ * Uses {@link TagService} to access Data Base through business-logic layer.
+ *
+ * @author Boris Prokhorenko
+ * @see Tag
+ * @see TagService
+ */
 @RestController
 @RequestMapping(value = "/tags")
 public class TagController {
@@ -18,26 +27,48 @@ public class TagController {
         this.service = service;
     }
 
+    /**
+     * Method allows getting {@link Tag} from DB by its id
+     *
+     * @param id - primary key to search {@link Tag} entity object in DB
+     * @return {@link Tag} entity object from DB
+     */
     @GetMapping(value = "/{id}")
     public Tag getTagById(@PathVariable Long id) {
         return service.getTagById(id);
     }
 
+    /**
+     * Method allows getting all {@link Tag} entity objects from DB
+     *
+     * @return {@link List} of {@link Tag} entity objects from DB
+     */
     @GetMapping(value = "/")
     public List<Tag> getAllTags() {
         return service.getAllTags();
     }
 
+    /**
+     * Method allows creating {@link Tag} in DB by its name
+     *
+     * @param name - name of tag which will be created
+     * @return {@link Tag} object, which you created
+     */
     @PostMapping(value = "/{name}")
     public Tag createTag(@PathVariable String name) {
         Tag tag = new Tag(name);
         return service.createTag(tag);
     }
 
+    /**
+     * Method allows deleting {@link Tag} from DB by its id
+     *
+     * @param id - primary key to search {@link Tag} entity object in DB
+     */
     @DeleteMapping(value = "/{id}")
     public @ResponseBody
     HttpStatus deleteTag(@PathVariable Long id) {
-         service.deleteTag(id);
+        service.deleteTag(id);
         return HttpStatus.OK;
     }
 
